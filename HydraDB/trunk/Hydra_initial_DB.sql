208381ff44771b798c69e04eb8f1e24d9aaedba8
philipp <philipp@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2013-07-15 11:50:49 +0000

    Added initial database schema (not yet complete).
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@2 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

---
+++ HydraDB/trunk/Hydra_initial_DB.sql
@@ -0,0 +1,167 @@
+DROP DATABASE IF EXISTS hydra_initial;
+CREATE DATABASE hydra_initial;
+
+USE hydra_initial;
+
+/* Project network and scenearios */
+
+CREATE TABLE tProject (
+    project_id INT NOT NULL PRIMARY KEY,
+    project_name VARCHAR(45),
+    project_description VARCHAR(1000)
+);
+
+CREATE TABLE tNetwork (
+    network_id INT NOT NULL PRIMARY KEY,
+    network_name VARCHAR(45),
+    network_description VARCHAR(1000),
+    project_id INT,
+    FOREIGN KEY (project_id) REFERENCES tProject(project_id)
+);
+
+CREATE TABLE tNode (
+    node_id INT NOT NULL PRIMARY KEY,
+    node_type VARCHAR(45)
+);
+
+CREATE TABLE tLink (
+    link_id INT PRIMARY KEY,
+    link_type VARCHAR(45),
+    network_id INT,
+    node_1_id INT,
+    node_2_id INT,
+    FOREIGN KEY (network_id) REFERENCES tNetwork(network_id),
+    FOREIGN KEY (node_1_id) REFERENCES tNode(node_id),
+    FOREIGN KEY (node_2_id) REFERENCES tNode(node_id)
+);
+
+CREATE TABLE tScenario (
+    scenario_id INT NOT NULL PRIMARY KEY,
+    scenario_name VARCHAR(45),
+    scenario_description VARCHAR(1000),
+    network_id INT,
+    FOREIGN KEY (network_id) REFERENCES tNetwork(network_id)
+);
+
+/* Constraints */
+
+CREATE TABLE tConstraintItem (
+    constraint_id INT,
+    item_id INT NOT NULL PRIMARY KEY,
+    resource_attr_id INT
+);
+
+CREATE TABLE tConstraintGroup (
+    constraint_id INT,
+    group_id INT NOT NULL PRIMARY KEY,
+    ref_key VARCHAR(45),
+    ref_id_1 INT,
+    ref_id_2 INT,
+    op VARCHAR(10),
+    FOREIGN KEY (ref_id_1) REFERENCES tConstraintItem(item_id),
+    FOREIGN KEY (ref_id_2) REFERENCES tConstraintItem(item_id)
+);
+
+CREATE TABLE tConstraint (
+    constraint_id INT NOT NULL PRIMARY KEY,
+    scenario_id INT,
+    group_id INT,
+    constant DOUBLE,
+    op VARCHAR(10),
+    FOREIGN KEY (scenario_id) REFERENCES tScenario(scenario_id),
+    FOREIGN KEY (group_id) REFERENCES tConstraintGroup(group_id)
+);
+
+ALTER TABLE tConstraintGroup ADD FOREIGN KEY (constraint_id) REFERENCES tConstraint(constraint_id);
+
+/* Data representation */
+
+CREATE TABLE tDescriptor (
+    data_id INT NOT NULL PRIMARY KEY,
+    desc_val VARCHAR(45)
+);
+
+CREATE TABLE tTimeSeries (
+    data_id INT NOT NULL PRIMARY KEY,
+    ts_time DATETIME,
+    ts_value DOUBLE
+);
+
+CREATE TABLE tTimeSeriesArray (
+    ts_array_id INT NOT NULL PRIMARY KEY,
+    ts_array_data BLOB
+);
+
+CREATE TABLE tEquallySpacedTimeSeries (
+    data_id INT NOT NULL PRIMARY KEY,
+    start_time DATETIME,
+    frequency FLOAT,
+    ts_array_id INT,
+    FOREIGN KEY (ts_array_id) REFERENCES tTimeSeriesArray(ts_array_id)
+);
+
+CREATE TABLE tScalar (
+    data_id INT NOT NULL PRIMARY KEY,
+    param_value DOUBLE
+);
+
+CREATE TABLE tArray (
+    data_id INT NOT NULL PRIMARY KEY,
+    arr_x_dim INT,
+    arr_y_dim INT,
+    arr_z_dim INt,
+    arr_precision VARCHAR(45),
+    arr_endianness VARCHAR(45),
+    arr_data BLOB
+);
+
+CREATE TABLE tDataAttr (
+    d_attr_id INT NOT NULL PRIMARY KEY,
+    data_id INT,
+    data_type VARCHAR(45),
+    d_attr_name VARCHAR(45),
+    d_attr_val DOUBLE
+);
+
+CREATE TABLE tScenarioData (
+    data_id INT,
+    data_type VARCHAR(45),
+    data_units VARCHAR(45),
+    data_name VARCHAR(45),
+    PRIMARY KEY (data_id, data_type)
+);
+
+CREATE TABLE tResourceScenario (
+    data_id INT,
+    scenario_id INT,
+    resource_attr_id INT,
+    PRIMARY KEY (data_id, scenario_id),
+    FOREIGN KEY (scenario_id) REFERENCES tScenario(scenario_id),
+    FOREIGN KEY (data_id) REFERENCES tScenarioData(data_id)
+);
+
+/* Attributes */
+
+CREATE TABLE tAttr (
+    attr_id INT NOT NULL PRIMARY KEY,
+    attr_name VARCHAR(45)
+);
+
+CREATE TABLE tAttrGroup (
+    attr_group_id INT NOT NULL PRIMARY KEY,
+    group_name VARCHAR(45)
+);
+
+CREATE TABLE tAttrGroupItem (
+    attr_id INT,
+    attr_group_id INT,
+    FOREIGN KEY (attr_id) REFERENCES tAttr(attr_id),
+    FOREIGN KEY (attr_group_id) REFERENCES tAttrGroup(attr_group_id)
+);
+
+CREATE TABLE tAttrMap (
+    attr_id_a INT,
+    attr_id_b INT,
+    FOREIGN KEY (attr_id_a) REFERENCES tAttr(attr_id),
+    FOREIGN KEY (attr_id_b) REFERENCES tAttr(attr_id)
+);
