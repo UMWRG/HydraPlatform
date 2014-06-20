43bd16a6ff2cc53b40e33f96336f3a447ef61c04
sknox <sknox@5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b>
2014-06-20 02:45:59 +0000

    SQLAlchemy integration commit 1.
    git-svn-id: svn+ssh://ec2-54-229-95-247.eu-west-1.compute.amazonaws.com/home/svn/svnrepository/HYDRA@438 5b2e1b5e-a1f6-4386-8dd1-2f88c1042e8b

--- HydraDB/trunk/HydraDB.sql
+++ HydraDB/trunk/HydraDB.sql
@@ -8,19 +8,20 @@ USE hydradb;
 
 CREATE TABLE tUser (
     user_id  INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username varchar(45) NOT NULL,
+    username varchar(60) NOT NULL,
     password varchar(1000) NOT NULL,
     display_name varchar(60) NOT NULL default '', 
     last_login DATETIME,
     last_edit  DATETIME,
+    UNIQUE (username),
     cr_date  TIMESTAMP default localtimestamp
 );
 create index iUser on tUser(username);
 
 CREATE TABLE tRole (
     role_id   INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    role_code VARCHAR(45) NOT NULL,
-    role_name VARCHAR(45) NOT NULL,
+    role_code VARCHAR(60) NOT NULL,
+    role_name VARCHAR(60) NOT NULL,
     cr_date  TIMESTAMP default localtimestamp,
     UNIQUE (role_code)
 );
@@ -28,8 +29,8 @@ create index iRole1 on tRole(role_code);
 
 CREATE TABLE tPerm (
     perm_id   INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    perm_code VARCHAR(45) NOT NULL,
-    perm_name VARCHAR(45) NOT NULL,
+    perm_code VARCHAR(60) NOT NULL,
+    perm_name VARCHAR(60) NOT NULL,
     cr_date  TIMESTAMP default localtimestamp,
     UNIQUE (perm_code),
     INDEX (perm_code)
@@ -54,28 +55,11 @@ CREATE TABLE tRolePerm (
     FOREIGN KEY (role_id) REFERENCES tRole(role_id)
 );
 
-CREATE TABLE tOwner (
-    user_id  INT NOT NULL,
-    ref_key  VARCHAR(45) NOT NULL,
-    ref_id   INT NOT NULL,
-    cr_date  TIMESTAMP default localtimestamp,
-    view     VARCHAR(1) NOT NULL,
-    edit     VARCHAR(1) NOT NULL,
-    share     VARCHAR(1) NOT NULL,
-    PRIMARY KEY (user_id, ref_key, ref_id),
-    CHECK (ref_key in ('PROJECT', 'NETWORK', 'DATASET')),
-    CHECK (view in ('Y','N')),
-    CHECK (edit in ('Y','N')),
-    CHECK (share in ('Y','N')),
-    FOREIGN KEY (user_id) REFERENCES tUser(user_id)
-);
-CREATE INDEX iOwner_1 ON tOwner (ref_key, ref_id);
-
 /* Project network and scenearios */
 
 CREATE TABLE tProject (
     project_id          INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    project_name        VARCHAR(45)   NOT NULL,
+    project_name        VARCHAR(60)   NOT NULL,
     project_description VARCHAR(1000),
     status              VARCHAR(1) default 'A' NOT NULL,
     cr_date  TIMESTAMP default localtimestamp,
@@ -89,7 +73,7 @@ insert into tProject (project_name) values ('Default Project');
 
 CREATE TABLE tNetwork (
     network_id          INT          NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    network_name        VARCHAR(45)  NOT NULL,
+    network_name        VARCHAR(60)  NOT NULL,
     network_description VARCHAR(1000),
     network_layout      TEXT,
     project_id          INT          NOT NULL,
@@ -109,7 +93,7 @@ CREATE TABLE tNode (
     node_id          INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     network_id       INT         NOT NULL,
     node_description VARCHAR(1000),
-    node_name        VARCHAR(45) NOT NULL,
+    node_name        VARCHAR(60) NOT NULL,
     status           VARCHAR(1) default 'A' NOT NULL,
     node_x           DOUBLE,
     node_y           DOUBLE,
@@ -126,7 +110,7 @@ CREATE TABLE tLink (
     status          VARCHAR(1) default 'A' NOT NULL,
     node_1_id       INT          NOT NULL,
     node_2_id       INT          NOT NULL,
-    link_name       VARCHAR(45),
+    link_name       VARCHAR(60),
     link_description VARCHAR(1000),
     link_layout     TEXT,
     cr_date  TIMESTAMP default localtimestamp,
@@ -140,7 +124,7 @@ CREATE TABLE tLink (
 
 CREATE TABLE tScenario (
     scenario_id          INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    scenario_name        VARCHAR(45)   NOT NULL,
+    scenario_name        VARCHAR(60)   NOT NULL,
     scenario_description VARCHAR(1000),
     status               VARCHAR(1) default 'A' NOT NULL,
     network_id           INT,
@@ -163,7 +147,7 @@ insert into tScenario (network_id, scenario_name) values (1, 'Project Scenario')
 
 CREATE TABLE tResourceGroup (
     group_id          INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    group_name        VARCHAR(45) NOT NULL,
+    group_name        VARCHAR(60) NOT NULL,
     group_description VARCHAR(1000),
     status            VARCHAR(1),
     cr_date           TIMESTAMP default localtimestamp,
@@ -174,46 +158,56 @@ CREATE TABLE tResourceGroup (
 
 CREATE TABLE tResourceGroupItem(
     item_id            INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    ref_id             INT NOT NULL,
-    ref_key            VARCHAR(45) NOT NULL,
+    node_id            INT,
+    link_id            INT,
+    subgroup_id        INT,
+    ref_key            VARCHAR(60) NOT NULL,
     group_id           INT NOT NULL,
     scenario_id        INT NOT NULL,
     FOREIGN KEY (group_id) REFERENCES tResourceGroup(group_id),
     FOREIGN KEY (scenario_id) REFERENCES tScenario(scenario_id),
-    CHECK (ref_key in ('GROUP', 'NODE', 'LINK', 'NETWORK')),
-    UNIQUE (scenario_id, group_id, ref_key, ref_id)
+    CHECK (ref_key in ('GROUP', 'NODE', 'LINK')),
+    FOREIGN KEY (node_id)    REFERENCES tNode(node_id),
+    FOREIGN KEY (link_id)    REFERENCES tLink(link_id),
+    FOREIGN KEY (subgroup_id)  REFERENCES tResourceGroup(group_id),
+    CHECK ((subgroup_id is not null and node_id is null and link_id is null) or (subgroup_id is null and node_id is not null and link_id is null) or (subgroup_id is null and node_id is null and link_id is not null))
 );
 
 CREATE TABLE tDataset (
     dataset_id  INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    data_id     INT         NOT NULL,
-    data_type   VARCHAR(45) NOT NULL,
-    data_units  VARCHAR(45),
-    data_dimen  VARCHAR(45),
-    data_name   VARCHAR(45) NOT NULL,
+    data_type   VARCHAR(60) NOT NULL,
+    data_units  VARCHAR(60),
+    data_dimen  VARCHAR(60),
+    data_name   VARCHAR(60) NOT NULL,
     data_hash   BIGINT      NOT NULL,
     cr_date     TIMESTAMP default localtimestamp,
     created_by  INT,
     locked      VARCHAR(1) NOT NULL default 'N',
+
+    start_time  DECIMAL(30, 20) UNSIGNED,
+    frequency   DOUBLE,
+    value       BLOB,
+
     FOREIGN KEY (created_by) REFERENCES tUser(user_id),
     constraint chk_type check (data_type in ('descriptor', 'timeseries',
     'eqtimeseries', 'scalar', 'array')),
-    CHECK (locked in ('Y', 'N'))
+    CHECK (locked in ('Y', 'N')),
+    UNIQUE (data_hash)
 );
 
 /* Attributes */
 
 CREATE TABLE tAttr (
     attr_id    INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    attr_name  VARCHAR(45) NOT NULL,
-    attr_dimen VARCHAR(45),
+    attr_name  VARCHAR(60) NOT NULL,
+    attr_dimen VARCHAR(60),
     cr_date  TIMESTAMP default localtimestamp,
     UNIQUE (attr_name, attr_dimen)
 );
 
 CREATE TABLE tTemplate (
     template_id   INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    template_name VARCHAR(45) NOT NULL,
+    template_name VARCHAR(60) NOT NULL,
     layout        TEXT,
     UNIQUE(template_name)
 );
@@ -223,9 +217,9 @@ insert into tTemplate (template_name) values ('Default');
 CREATE TABLE tTemplateType(
     type_id       INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     type_name     VARCHAR(45) NOT NULL,
-    resource_type VARCHAR(45),
+    resource_type VARCHAR(60),
     template_id   INT,
-    alias         varchar(45),
+    alias         varchar(60),
     layout        TEXT,
     FOREIGN KEY (template_id) REFERENCES tTemplate(template_id),
     UNIQUE(template_id, type_name)
@@ -236,8 +230,8 @@ CREATE TABLE tTypeAttr (
     type_id INT NOT NULL,
     default_dataset_id  INT,
     attr_is_var VARCHAR(1) default 'N',
-    data_type   VARCHAR(45),
-    dimension   VARCHAR(45),
+    data_type   VARCHAR(60),
+    dimension   VARCHAR(60),
     PRIMARY KEY (attr_id, type_id),
     FOREIGN KEY (attr_id) REFERENCES tAttr(attr_id),
     FOREIGN KEY (type_id) REFERENCES tTemplateType(type_id),
@@ -256,99 +250,54 @@ CREATE TABLE tAttrMap (
 );
 
 CREATE TABLE tResourceType (
-    ref_key          VARCHAR(45) NOT NULL,
-    ref_id           INT         NOT NULL,
+    resource_type_id INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    ref_key          VARCHAR(60) NOT NULL,
+    network_id       INT,
+    node_id          INT,
+    link_id          INT,
+    group_id         INT,
     type_id      int         NOT NULL,
     FOREIGN KEY (type_id) REFERENCES tTemplateType(type_id),
-    PRIMARY KEY (ref_key, ref_id, type_id),
-    CHECK (ref_key in ('GROUP', 'NODE', 'LINK', 'NETWORK'))
+    CHECK (ref_key in ('GROUP', 'NODE', 'LINK', 'NETWORK')),
+    FOREIGN KEY (network_id) REFERENCES tNetwork(network_id),
+    FOREIGN KEY (node_id)    REFERENCES tNode(node_id),
+    FOREIGN KEY (link_id)    REFERENCES tLink(link_id),
+    FOREIGN KEY (group_id)   REFERENCES tResourceGroup(group_id),
+    CHECK ((group_id is not null and network_id is null and node_id is null and link_id is null) or (group_id is null and network_id is not null and node_id is null and link_id is null) or (group_id is null and network_id is null and node_id is not null and link_id is null) or (group_id is null and network_id is null and node_id is null and link_id is not null))
 );
 
 CREATE TABLE tResourceAttr (
     resource_attr_id INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
     attr_id          INT         NOT NULL,
-    ref_key          VARCHAR(45) NOT NULL,
-    ref_id           INT         NOT NULL,
+    ref_key          VARCHAR(60) NOT NULL,
+    project_id       INT,
+    network_id       INT,
+    node_id          INT,
+    link_id          INT,
+    group_id         INT,
     attr_is_var      VARCHAR(1)  NOT NULL default 'N',
     FOREIGN KEY (attr_id) REFERENCES tAttr(attr_id),
-    CHECK (ref_key in ('GROUP', 'NODE', 'LINK', 'NETWORK'))
-);
-CREATE UNIQUE INDEX iResourceAttr_1 ON tResourceAttr (attr_id, ref_key, ref_id);
-CREATE INDEX iResourceAttr_2 ON tResourceAttr (ref_key, ref_id);
-
-/* Constraints */
-
-CREATE TABLE tConstraint (
-    constraint_id INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    scenario_id   INT         NOT NULL,
-    group_id      INT,
-    constant      DOUBLE      NOT NULL,
-    op            VARCHAR(10) NOT NULL,
-    FOREIGN KEY (scenario_id) REFERENCES tScenario(scenario_id)
-);
-
-CREATE TABLE tConstraintItem (
-    item_id          INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    constraint_id    INT NOT NULL,
-    resource_attr_id INT,
-    constant         BLOB,
-    CHECK ((constant is null and resource_attr_id is not null) or (constant is not null and resource_attr_id is null)), 
-    FOREIGN KEY (constraint_id) REFERENCES tConstraint(constraint_id),
-    FOREIGN KEY (resource_attr_id) REFERENCES tResourceAttr(resource_Attr_id)
-);
-
-CREATE TABLE tConstraintGroup (
-    group_id      INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    constraint_id INT         NOT NULL,
-    ref_key_1     VARCHAR(45) NOT NULL,
-    ref_id_1      INT         NOT NULL,
-    ref_key_2     VARCHAR(45),
-    ref_id_2      INT,
-    op            VARCHAR(10),
-    CHECK (ref_key_1 in ('GRP', 'ITEM')),
-    CHECK (ref_key_2 is null or ref_key_2 in ('GRP', 'ITEM')),
-    FOREIGN KEY (constraint_id) REFERENCES tConstraint(constraint_id)
+    CHECK (ref_key in ('GROUP', 'NODE', 'LINK', 'NETWORK', 'PROJECT')),
+    FOREIGN KEY (network_id) REFERENCES tNetwork(network_id),
+    FOREIGN KEY (node_id)    REFERENCES tNode(node_id),
+    FOREIGN KEY (link_id)    REFERENCES tLink(link_id),
+    FOREIGN KEY (group_id)   REFERENCES tResourceGroup(group_id),
+    CHECK ((group_id is not null and network_id is null and node_id is null and link_id is null ) or (group_id is null and network_id is not null and node_id is null and link_id is null) or (group_id is null and network_id is null and node_id is not null and link_id is null) or (group_id is null and network_id is null and node_id is null and link_id is not null))
 );
 
 /* Data representation */
 
-CREATE TABLE tDescriptor (
-    data_id  INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    desc_val VARCHAR(1000) NOT NULL
-);
-
-CREATE TABLE tTimeSeries (
-    data_id  INT      NOT NULL PRIMARY KEY AUTO_INCREMENT
-);
-
 CREATE TABLE tTimeSeriesData(
-    data_id  INT         NOT NULL,
-    ts_time  DECIMAL(30, 20) UNSIGNED NOT NULL,
-    ts_value BLOB        NOT NULL,
-    PRIMARY KEY (data_id, ts_time),
-    FOREIGN KEY (data_id) references tTimeSeries(data_id)
-);
-
-CREATE TABLE tEqTimeSeries (
-    data_id       INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    start_time    DECIMAL(30, 20) UNSIGNED NOT NULL,
-    frequency     DOUBLE      NOT NULL,
-    arr_data      BLOB        NOT NULL
-);
-
-CREATE TABLE tScalar (
-    data_id     INT    NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    param_value DOUBLE NOT NULL
-);
-
-CREATE TABLE tArray (
-    data_id        INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    arr_data       BLOB        NOT NULL
+    dataset_id  INT NOT NULL,
+    ts_time     DECIMAL(30, 20) UNSIGNED NOT NULL,
+    ts_value    BLOB  NOT NULL,
+    PRIMARY KEY (dataset_id, ts_time),
+    FOREIGN KEY (dataset_id) references tDataset(dataset_id)
 );
 
 CREATE TABLE tDatasetGroup (
     group_id    INT     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    group_name  VARCHAR(45) NOT NULL,
+    group_name  VARCHAR(60) NOT NULL,
     cr_date     TIMESTAMP default localtimestamp,
     UNIQUE (group_name)
 );
@@ -363,7 +312,7 @@ CREATE TABLE tDatasetGroupItem (
 
 CREATE TABLE tMetadata (
     dataset_id  INT         NOT NULL,
-    metadata_name VARCHAR(45) NOT NULL,
+    metadata_name VARCHAR(60) NOT NULL,
     metadata_val  BLOB      NOT NULL,
     PRIMARY KEY (dataset_id, metadata_name),
     FOREIGN KEY (dataset_id) REFERENCES tDataset(dataset_id)
@@ -377,6 +326,52 @@ CREATE TABLE tResourceScenario (
     FOREIGN KEY (scenario_id) REFERENCES tScenario(scenario_id),
     FOREIGN KEY (dataset_id) REFERENCES tDataset(dataset_id),
     FOREIGN KEY (resource_attr_id) REFERENCES tResourceAttr(resource_attr_id)
+);
+
+/*Ownership*/
+CREATE TABLE tProjectOwner (
+    user_id  INT NOT NULL,
+    project_id INT,
+    view     VARCHAR(1) NOT NULL,
+    edit     VARCHAR(1) NOT NULL,
+    share     VARCHAR(1) NOT NULL,
+    cr_date  TIMESTAMP default localtimestamp,
+    PRIMARY KEY (user_id, project_id),
+    CHECK (view in ('Y','N')),
+    CHECK (edit in ('Y','N')),
+    CHECK (share in ('Y','N')),
+    FOREIGN KEY (user_id) REFERENCES tUser(user_id),
+    FOREIGN KEY (project_id) REFERENCES tProject(project_id)
+);
+
+CREATE TABLE tNetworkOwner (
+    user_id  INT NOT NULL,
+    network_id INT,
+    view     VARCHAR(1) NOT NULL,
+    edit     VARCHAR(1) NOT NULL,
+    share     VARCHAR(1) NOT NULL,
+    cr_date  TIMESTAMP default localtimestamp,
+    PRIMARY KEY (user_id, network_id),
+    CHECK (view in ('Y','N')),
+    CHECK (edit in ('Y','N')),
+    CHECK (share in ('Y','N')),
+    FOREIGN KEY (user_id) REFERENCES tUser(user_id),
+    FOREIGN KEY (network_id) REFERENCES tNetwork(network_id)
+);
+
+CREATE TABLE tDatasetOwner (
+    user_id  INT NOT NULL,
+    dataset_id INT,
+    view     VARCHAR(1) NOT NULL,
+    edit     VARCHAR(1) NOT NULL,
+    share     VARCHAR(1) NOT NULL,
+    cr_date  TIMESTAMP default localtimestamp,
+    PRIMARY KEY (user_id, dataset_id),
+    CHECK (view in ('Y','N')),
+    CHECK (edit in ('Y','N')),
+    CHECK (share in ('Y','N')),
+    FOREIGN KEY (user_id) REFERENCES tUser(user_id),
+    FOREIGN KEY (dataset_id) REFERENCES tDataset(dataset_id)
 );
 
 source data.sql;
