cc34f67f70d0cf820a07498754094f351fae68e8
stephen <s.knox@ucl.ac.uk>
2015-11-23 15:32:07 +0000

    Add a time map file, which is utf8 encoded so that you can add pudb into hydra_dateutil.

--- HydraLib/python/HydraLib/hydra_dateutil.py
+++ HydraLib/python/HydraLib/hydra_dateutil.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 
 # (c) Copyright 2013, 2014, University of Manchester
 #
@@ -32,62 +31,7 @@ FORMAT = "%Y-%m-%d %H:%M:%S.%f"
     A mapping from commonly used time periods to the appropriate hydra-compatible
     time period abbreviation
 """
-time_map = {
-    'picosecond'  : 'ps',
-    'picoseconds' : 'ps',
-    'picosec'     : 'ps',
-    'picosecs'    : 'ps',
-    'ps'          : 'ps',
-
-    'nanosecond'  : 'ns',
-    'nanoseconds' : 'ns',
-    'nanosecs'    : 'ns',
-    'nanosec'     : 'ns',
-    'ns'          : 'ns',
-
-    'microsecond'  : '탎',
-    'microseconds' : '탎',
-    'microsec'     : '탎',
-    'microsecs'    : '탎',
-    '탎'           : '탎',
-
-    'millisecond'   : 'ms',
-    'milliseconds'  : 'ms',
-    'millisec'      : 'ms',
-    'millisecs'     : 'ms',
-    'ms'            : 'ms',
-
-    'second'  : 's',
-    'seconds' : 's',
-    'sec'     : 's',
-    'secs'    : 's',
-    's'       : 's',
-    
-    'minute'  : 'min',
-    'minutes' : 'min',
-    'min'     : 'min',
-    'mins'    : 'min',
-    'm'       : 'min',
-
-    'h'     : 'h',
-    'hour'  : 'h',
-    'hours' : 'h',
-
-    'd'    : 'day',
-    'day'  : 'day',
-    'days' : 'day',
-
-    'mon'    : 'mon',
-    'month'  : 'mon',
-    'months' : 'mon',
-    'mons'   : 'mon',
-
-    'y'    : 'yr',
-    'yr'   : 'yr',
-    'year' : 'yr',
-    'years': 'yr',
-    'yrs'  : 'yr',
-}
+from time_map import time_map
 
 
 def get_time_period(period_name):
