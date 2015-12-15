7f825f2abacbf257601aea8661eca32f4a246eb3
stephen <s.knox@ucl.ac.uk>
2015-12-15 00:35:43 +0000

    Add a ref to the front page of doc and remove copyright

--- doc/conf.py
+++ doc/conf.py
@@ -48,7 +48,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'HydraPlatform'
-copyright = u'2015, CH2M Hill Halcrow and The University of Manchester'
+#copyright = u'2015, CH2M Hill Halcrow and The University of Manchester'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
