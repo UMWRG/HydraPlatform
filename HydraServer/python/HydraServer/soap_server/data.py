71e24a71bd73810525c41a596da032f94376d986
Stephen <knoxsp@gmail.com>
2016-01-06 09:02:21 +0000

    Add docstrings to data functions

--- HydraServer/python/HydraServer/soap_server/data.py
+++ HydraServer/python/HydraServer/soap_server/data.py
@@ -35,6 +35,23 @@ class DataService(HydraService):
     def add_dataset(ctx, dataset):
         """
            Add a single dataset. Return the new dataset with a dataset ID.
+                .. code-block:: python
+
+                    (Dataset){
+                        value     = 123,
+                        unit      = 'm^3', 
+                        dimension = 'Volume', 
+                        name      = 'Storage Capacity',
+                        type      = 'scalar', #(others are 'descriptor', 'array' and 'timeseries')
+                        metadata  = "{'measured_by':'John Doe'}", #Json encoded python dictionary
+                    }
+
+           Args:
+               dataset (Dataset): The dataset complex model (see above)
+
+           Returns:
+               Dataset: The new dataset object, complete with ID
+
         """
         value = dataset.parse_value()
         metadata = dataset.get_metadata_as_dict(user_id=ctx.in_header.user_id)
@@ -52,7 +69,16 @@ class DataService(HydraService):
     @rpc(SpyneArray(Integer32), _returns=SpyneArray(Dataset))
     def get_datasets(ctx, dataset_ids):
         """
-            Get a list of datasets, by ID
+        Get a list of datasets, by ID
+        
+        Args:
+            dataset_ids (List(int)): A list of dataset IDs
+
+        Returns:
+            List(Dataset): The corresponding list of datasets. A subset will be returned if not all datasets are available.
+
+        Raises:
+            ResourceNotFoundError: If none of the requested datasets were found.
         """
         datasets = data.get_datasets(dataset_ids, **ctx.in_header.__dict__)
         ret_datasets = [Dataset(d) for d in datasets]
@@ -61,17 +87,40 @@ class DataService(HydraService):
     @rpc(Integer, _returns=Dataset)
     def get_dataset(ctx, dataset_id):
         """
-            Get a single dataset, by ID
+        Get a single dataset, by ID
+
+        Args:
+            dataset_id (int): THe ID of the requested dataset
+
+        Returns:
+            Dataset: The dataset complex model
+
+        Raises:
+            ResourceNotFoundError: If the dataset does not exist.
         """
+
         dataset_i = data.get_dataset(dataset_id, **ctx.in_header.__dict__)
+        
         return Dataset(dataset_i)
 
     @rpc(Integer, _returns=Dataset)
     def clone_dataset(ctx, dataset_id):
         """
-            Clone a single dataset, by ID
+        Clone a single dataset, by ID
+    
+        Args:
+            dataset_id (int): THe ID of the dataset to be cloned
+
+        Returns:
+            Dataset: The newly cloned dataset complex model
+
+        Raises:
+            ResourceNotFoundError: If the dataset does not exist.
+
         """
+
         dataset_i = data.clone_dataset(dataset_id, **ctx.in_header.__dict__)
+
         return Dataset(dataset_i)
 
 
@@ -99,7 +148,31 @@ class DataService(HydraService):
                 page_start,
                 page_size):
         """
-            Search for datadets that satisfy the criteria specified.
+        Search for datadets that satisfy the criteria specified.
+        By default, returns a max of 2000 datasets. To return datasets from 2001 onwards,
+        set page_start to 2001. 
+    
+        Args:
+            dataset_id      (int)    : The ID of the dataset
+            name            (string) : The name of the dataset
+            collection_name (string) : Search for datsets in a collection with this name
+            data_type       (string) : 'scalar', 'descriptor', 'array', 'timeseries'
+            dimension       (string) : Datasets with this dimension
+            unit            (string) : Datasets with this unit.
+            scenario_id     (int)    : Datasets in this scenraio
+            metadata_name   (string) : Datasets that have this metadata
+            metadata_val    (string) : Datasets that have this metadata value
+            attr_id         (int)    : Datasts that are associated with this attribute via resource scenario & resource attribute
+            type_id         (int)    : Datasets that are associated with this type via resource scenario -> resource attribute -> attribute -> type
+            unconnected     (char)   : Datasets that are not in any scenarios
+            inc_metadata    (char) (default 'N')   : Return metadata with retrieved datasets. 'Y' gives a performance hit.
+            inc_val         (char) (default 'N')  : Include the value with the dataset. 'Y' gives a performance hit
+            page_start      (int)    : Return datasets from this point (ex: from index 2001 of 10,000)
+            page_size       (int)    : Return this number of datasets in one go. default is 2000.
+
+        Returns:
+            List(Dataset): The datasets matching all the specified criteria.
+
         """
         datasets = data.search_datasets(dataset_id,
                                      name,
@@ -128,7 +201,13 @@ class DataService(HydraService):
     @rpc(Integer(max_occurs="unbounded"), _returns=Unicode)
     def get_metadata(ctx, dataset_ids):
         """
-            Get the metadata for a dataset or list of datasets
+        Get the metadata for a dataset or list of datasets
+
+        Args:
+            dataset_ids (List(int)): The list of dataset IDS that you want metadata for
+
+        Returns:
+            (string): A dictionary keyed on metadata name, dumped as a json string.
         """
 
         if type(dataset_ids) == int:
@@ -145,6 +224,12 @@ class DataService(HydraService):
     def bulk_insert_data(ctx, bulk_data):
         """
             Insert sereral pieces of data at once.
+
+            Args:
+                bulk_data (List(Dataset)): A list of Dataset complex models
+
+            Returns:
+                List(int): A list of new dataset IDS
         """
         datasets = data.bulk_insert_data(bulk_data, **ctx.in_header.__dict__)
 
@@ -153,7 +238,14 @@ class DataService(HydraService):
     @rpc(_returns=SpyneArray(DatasetCollection))
     def get_all_dataset_collections(ctx):
         """
-            Get all the dataset collections available. 
+        Get all the dataset collections available.
+
+        Args:
+            None
+
+        Returns:
+            List(DatasetCollection): A list of dataset collection objects, each containing references to all the datasets inside them.
+
         """
         dataset_colns = data.get_all_dataset_collections(**ctx.in_header.__dict__)
         all_colns = []
@@ -165,7 +257,17 @@ class DataService(HydraService):
     @rpc(Integer, Integer, _returns=Unicode)
     def add_dataset_to_collection(ctx, dataset_id, collection_id):
         """
-            Add a single dataset to a dataset collection.
+        Add a single dataset to a dataset collection.
+
+        Args:
+            dataset_id (int): The dataset to add to the collection
+            collection_id (int): The collection to receive the new dataset
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the dataset or collection do not exist
         """
 
         data.add_dataset_to_collection(dataset_id,
@@ -176,7 +278,17 @@ class DataService(HydraService):
     @rpc(SpyneArray(Integer32), Integer, _returns=Unicode)
     def add_datasets_to_collection(ctx, dataset_ids, collection_id):
         """
-            Add multiple datasets to a dataset collection.
+        Add multiple datasets to a dataset collection.
+
+        Args:
+            dataset_ids (Lsit(int)): The IDs of the datasets to add to the collection
+            collection_id (int): The collection to receive the new dataset
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the collection does not exist
         """
         data.add_datasets_to_collection(dataset_ids,
                                         collection_id,
@@ -186,7 +298,18 @@ class DataService(HydraService):
     @rpc(Integer, Integer, _returns=Unicode)
     def remove_dataset_from_collection(ctx, dataset_id, collection_id):
         """
-            Remove a single dataset to a dataset collection.
+        Remove a single dataset to a dataset collection.
+
+        Args:
+            dataset_id (int): The dataset to remove from the collection
+            collection_id (int): The collection to lose the dataset
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the dataset or collection do not exist
+
         """
         data.remove_dataset_from_collection(dataset_id,
                                              collection_id,
@@ -196,7 +319,17 @@ class DataService(HydraService):
     @rpc(Integer, Integer, _returns=Unicode(pattern='[YN]'))
     def check_dataset_in_collection(ctx, dataset_id, collection_id):
         """
-            Check whether a dataset is contained inside a collection
+        Check whether a dataset is contained inside a collection
+   
+        Args:
+            dataset_id (int): The dataset being checked
+            collection_id (int): The collection to check in
+
+        Returns:
+            char: 'Y' or 'N'
+
+        Raises:
+            ResourceNotFoundError: If the collection does not exist
         """
         
         result = data.check_dataset_in_collection(dataset_id,
@@ -207,8 +340,17 @@ class DataService(HydraService):
     @rpc(Integer, _returns=DatasetCollection)
     def get_dataset_collection(ctx, collection_id):
         """
-            Get a single dataset collection, by ID.
-            :param Collection ID.
+        Get a single dataset collection, by ID.
+        
+        Args:
+            collection_id (int): The collection to retrieve
+
+        Returns:
+            DatasetCollection: A dataset collection complex model
+
+        Raises:
+            ResourceNotFoundError: If the collection does not exist
+
         """
 
         dataset_coln_i = data.get_dataset_collection(collection_id,
@@ -218,8 +360,18 @@ class DataService(HydraService):
     @rpc(Integer, _returns=Unicode)
     def delete_dataset_collection(ctx, collection_id):
         """
-            Delete a single dataset collection, by ID.
-            :param Collection ID.
+        Delete a single dataset collection, by ID.
+   
+        Args:
+            collection_id (int): The collection to delete
+
+        Returns:
+            string: 'OK' 
+
+        Raises:
+            ResourceNotFoundError: If the collection does not exist
+
+
         """
 
         data.delete_dataset_collection(collection_id,
@@ -229,8 +381,17 @@ class DataService(HydraService):
     @rpc(Unicode, _returns=DatasetCollection)
     def get_dataset_collection_by_name(ctx, collection_name):
         """
-            Get all the dataset collections with the provided name.
-            :param A collection name.
+        Get all the dataset collections with the provided name.
+
+        Args:
+            collection_name (string): The name of the collection to retrieve
+
+        Returns:
+            DatasetCollection: A dataset collection complex model, containing a list of DatasetCollectionItem complex models.
+
+        Raises:
+            ResourceNotFoundError: If the collection does not exist
+
         """
         dataset_coln_i = data.get_dataset_collection_by_name(collection_name,
                                                              **ctx.in_header.__dict__)
@@ -239,11 +400,16 @@ class DataService(HydraService):
     @rpc(DatasetCollection, _returns=DatasetCollection)
     def add_dataset_collection(ctx, collection):
         """
-            Add a dataset collection:
-            The name of the collection does NOT need to be unique, so be careful
-            with the naming to ensure the collection is searchable later.
+        Add a dataset collection:
+        The name of the collection does NOT need to be unique, so be careful
+        with the naming to ensure the collection is searchable later.
 
-            :param DatasetCollection object, containing a list of DatasetCollectionItem objects
+        Args:
+            collection (DatasetCollection): A DatasetCollection complex model containing a list of DatasetCollectionItem objects
+
+        Returns:
+            DatasetCollection: The same collection as was sent in, but with an ID
+
         """
 
         dataset_coln_i = data.add_dataset_collection(collection, **ctx.in_header.__dict__)
@@ -254,8 +420,13 @@ class DataService(HydraService):
     @rpc(Unicode, _returns=SpyneArray(DatasetCollection))
     def get_collections_like_name(ctx, collection_name):
         """
-            Get all the dataset collections with a name like the specified name
-            :param The collection name to search for.
+        Get all the dataset collections with a name like the specified name
+
+        Args:
+            collection_name (string): The collection name to search.
+
+        Returns:
+            List(DatasetCollection): All the collections with names similar to the specified name
         """
         collections = data.get_collections_like_name(collection_name, **ctx.in_header.__dict__)
         ret_collections = [DatasetCollection(g) for g in collections]
@@ -265,6 +436,12 @@ class DataService(HydraService):
     def get_collection_datasets(ctx, collection_id):
         """
             Get all the datasets from the collection with the specified name
+
+            Args:
+                collection_id (int): The collection whose dastasets we want to retrieve
+
+            Returns:
+                List(Dataset): A list of dastaset complex models, all of them in the collection specified
         """
         collection_datasets = data.get_collection_datasets(collection_id,
                                                  **ctx.in_header.__dict__)
@@ -277,6 +454,12 @@ class DataService(HydraService):
         """
             Update a piece of data directly, rather than through a resource
             scenario.
+
+            Args:
+                dataset (Dataset): A complex model representing an existing dataset which is to be updsated (must have an id)
+
+            Returns:
+                Dataset: The updated dataset
         """
         val = dataset.parse_value()
 
@@ -294,24 +477,70 @@ class DataService(HydraService):
         return Dataset(updated_dataset)
 
 
-    @rpc(Integer, _returns=Boolean)
+    @rpc(Integer, _returns=Unicode)
     def delete_dataset(ctx, dataset_id):
         """
             Removes a piece of data from the DB.
             CAUTION! Use with care, as this cannot be undone easily.
+
+            Args:
+                dataset_id (int): The ID of the dataset to be deleted.
+
+            Returns:
+                string: 'OK'
         """
-        success = True
         data.delete_dataset(dataset_id, **ctx.in_header.__dict__)
-        return success
+        return 'OK'
 
     @rpc(Integer, Unicode(min_occurs=0, max_occurs='unbounded'), _returns=AnyDict)
     def get_val_at_time(ctx, dataset_id, timestamps):
+        """
+        Get the value of the dataset at a specified time (s).
+        
+        - If the dataset is not a timeseries, just return the value
+        
+        - If the dataset is a timeseries, return the value within the timeseries
+        that is closest to the requested time(s). 
+        
+        - If the time specified occurs before the start of the timeseries, return None. 
+        
+        - If the time specified occurs after the end of the timeseries, return the last value in the timeseries.
+
+        Args:
+            dataset_id (int): The ID of the dataset being searched
+            timestamps (List(timestamps)): A list of timestamps to get values for.
+
+        Returns:
+            dict: A dictionary, keyed on the timestamps requested
+
+        """
         return data.get_val_at_time(dataset_id, 
                                     timestamps,
                                     **ctx.in_header.__dict__)
     
     @rpc(Integer32(min_occurs=0, max_occurs='unbounded'), Unicode(min_occurs=0, max_occurs='unbounded'), _returns=AnyDict)
     def get_multiple_vals_at_time(ctx, dataset_ids, timestamps):
+        """
+        Similar to get_val_at_time, but perform the action on multiple datasets at once
+        
+        - If the dataset is not a timeseries, just return the value
+        
+        - If the dataset is a timeseries, return the value within the timeseries
+        that is closest to the requested time(s). 
+        
+        - If the time specified occurs before the start of the timeseries, return None. 
+        
+        - If the time specified occurs after the end of the timeseries, return the last value in the timeseries.
+
+        Args:
+            dataset_ids (List(int)): The IDs of the datasets being searched
+            timestamps (List(timestamps)): A list of timestamps to get values for.
+
+        Returns:
+            dict: A dictionary, keyed on the dataset_id, then by the timestamps requested
+
+        """
+
         result = data.get_multiple_vals_at_time(dataset_ids, 
                                     timestamps,
                                     **ctx.in_header.__dict__)
@@ -319,6 +548,35 @@ class DataService(HydraService):
 
     @rpc(Integer,Unicode,Unicode,Unicode(values=['seconds', 'minutes', 'hours', 'days', 'months']), Decimal(default=1),_returns=AnyDict)
     def get_vals_between_times(ctx, dataset_id, start_time, end_time, timestep, increment):
+        """
+        Retrive data between two specified times within a timeseries. The times
+        need not be specified in the timeseries. This function will 'fill in the blanks'.
+
+        Two types of data retrieval can be done.
+
+        If the timeseries is timestamp-based, then start_time and end_time
+        must be datetimes and timestep must be specified (minutes, seconds etc).
+        'increment' reflects the size of the timestep -- timestep = 'minutes' and increment = 2
+        means 'every 2 minutes'.
+
+        If the timeseries is float-based (relative), then start_time and end_time
+        must be decimal values. timestep is ignored and 'increment' represents the increment
+        to be used between the start and end.
+        Ex: start_time = 1, end_time = 5, increment = 1 will get times at 1, 2, 3, 4, 5
+
+        Args:
+            dataset_id (int): The dataset being queried
+            start_time (string): The date or value from which to start the query
+            end_time   (string): The date or value that ends the query
+            timestep   Enum(string): 'seconds', 'minutes', 'hours', 'days', 'months':
+                The increment in time that the result will be in
+            increment  (decimal): The increment that the result will be in if the timeseries is not timestamp-based.
+
+        Returns:
+            (AnyDict): A dictionary, keyed on the newly created timestamps, which have been
+                        created from the start time and timesteps.
+
+        """
         return data.get_vals_between_times(dataset_id, 
                                            start_time, 
                                            end_time, 
@@ -328,6 +586,16 @@ class DataService(HydraService):
 
     @rpc(Unicode, _returns=Unicode)
     def check_json(ctx, json_string):
+        """
+        Check that an incoming data string is json serialisable.
+        Used for testing.
+
+        Args:
+            json_string (string): A json string to be tested for validity
+
+        Returns:
+            'OK' or '"Unable to process JSON string. error was:..."
+        """
         try:
             data.check_json(json_string)
         except Exception, e:
