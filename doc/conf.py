9608e26125e7b970ff1df235b5d5b22080cb2d7c
stephen <s.knox@ucl.ac.uk>
2016-01-18 13:36:35 +0000

    Test to see if altering the pythonpath will make the gh-pages build work properly

--- doc/conf.py
+++ doc/conf.py
@@ -17,12 +17,12 @@ import alabaster
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../'))
-sys.path.insert(0, os.path.abspath('../HydraServer/python'))
-sys.path.insert(0, os.path.abspath('../HydraServer/python/HydraServer/unittests'))
-sys.path.insert(0, os.path.abspath('../HydraPlugins/WaterML/ImportWML'))
-sys.path.insert(0, os.path.abspath('../HydraPlugins/CSVplugin/ImportCSV'))
-sys.path.insert(0, os.path.abspath('../HydraPlugins/CSVplugin/ExportCSV'))
+sys.path.insert(0, os.path.abspath('./'))
+sys.path.insert(0, os.path.abspath('./HydraServer/python'))
+sys.path.insert(0, os.path.abspath('./HydraServer/python/HydraServer/unittests'))
+sys.path.insert(0, os.path.abspath('./HydraPlugins/WaterML/ImportWML'))
+sys.path.insert(0, os.path.abspath('./HydraPlugins/CSVplugin/ImportCSV'))
+sys.path.insert(0, os.path.abspath('./HydraPlugins/CSVplugin/ExportCSV'))
 
 
 # -- General configuration -----------------------------------------------------
