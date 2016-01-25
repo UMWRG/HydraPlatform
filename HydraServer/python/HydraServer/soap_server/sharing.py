34f2e92270668fee932e8bc4ddca1f0e5fd4aaa1
stephen <s.knox@ucl.ac.uk>
2016-01-25 14:54:25 +0000

    docstrings added to sharing

--- HydraServer/python/HydraServer/soap_server/sharing.py
+++ HydraServer/python/HydraServer/soap_server/sharing.py
@@ -28,13 +28,22 @@ class SharingService(HydraService):
          String(pattern="[YN]"), String(pattern="[YN]", default='Y'), _returns=String())
     def share_network(ctx, network_id, usernames, read_only, share):
         """
-            Share a network with a list of users, identified by their usernames.
-            
-            The read_only flag ('Y' or 'N') must be set
-            to 'Y' to allow write access or sharing.
 
-            The share flat ('Y' or 'N') must be set to 'Y' to allow the 
-            project to be shared with other users 
+        Share a network with a list of users, identified by their usernames.
+
+        Args:
+            network_id (int): The ID of the network to share
+            usernames  (List(String)): THe list of usernames with whom to share the network
+            read_only  (string) (Y or N): Can the users edit as well as view the network?
+            share      (string) (optional) (Y or N): Can the users share the network with other users? This only goes 1 level deep, so if you share a network with a user and give them sharing ability, they cannot then set the 'share' flag to 'Y' when sharing with someone else.
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the network is not found
+            ResourceNotFoundError: If one of the usernames is incorrect or does not exist
+
         """
 
         sharing.share_network(network_id,
@@ -49,14 +58,23 @@ class SharingService(HydraService):
          String(pattern="[YN]"), String(pattern="[YN]"), _returns=String)
     def share_project(ctx, project_id, usernames, read_only, share):
         """
-            Share an entire project with a list of users, identifed by 
-            their usernames. 
-            
-            The read_only flag ('Y' or 'N') must be set
-            to 'Y' to allow write access or sharing.
 
-            The share flat ('Y' or 'N') must be set to 'Y' to allow the 
-            project to be shared with other users
+        Share an entire project with a list of users, identifed by their usernames.
+        All the projectt's networks will be shared also.
+            
+        Args:
+            project_id (int): The ID of the project to share
+            usernames  (List(String)): The list of usernames with whom to share the project
+            read_only  (string) (Y or N): Can the users edit as well as view the project?
+            share      (string) (optional) (Y or N): Can the users share the project with other users? This only goes 1 level deep, so if you share a project with a user and give them sharing ability, they cannot then set the 'share' flag to 'Y' when sharing with someone else.
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the project is not found
+            ResourceNotFoundError: If one of the usernames is incorrect or does not exist
+
         """
         sharing.share_project(project_id,
                               usernames,
@@ -70,12 +88,24 @@ class SharingService(HydraService):
          _returns=String)
     def set_project_permission(ctx, project_id, usernames, read, write, share):
         """
-            Set permissions on a project to a list of users, identifed by 
-            their usernames. 
+
+        Set permissions on a project to a list of users, identifed by their usernames. 
+        THIS ACTION IS THEN PERFORMED ON ALL THE PROJECT'S NETWORKS!
             
-            The read flag ('Y' or 'N') sets read access, the write
-            flag sets write access. If the read flag is 'N', then there is
-            automatically no write access or share access.
+        Args:
+            project_id (int): The ID of the project to share
+            usernames  (List(String)): The list of usernames with whom to share the project
+            read       (string) (Y or N): Can the users read the project?
+            write      (string) (Y or N): Can the users edit the project?
+            share      (string) (optional) (Y or N): Can the users share the project with other users? This only goes 1 level deep, so if you share a network with a user and give them sharing ability, they cannot then set the 'share' flag to 'Y' when sharing with someone else.
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the project is not found
+            ResourceNotFoundError: If one of the usernames is incorrect or does not exist
+
         """
         sharing.set_project_permission(project_id,
                                        usernames,
@@ -89,10 +119,26 @@ class SharingService(HydraService):
          _returns=String)
     def set_network_permission(ctx, network_id, usernames, read, write, share):
         """
-            Set permissions on a network to a list of users, identifed by 
-            their usernames. The read flag ('Y' or 'N') sets read access, the write
-            flag sets write access. If the read flag is 'N', then there is
-            automatically no write access or share access.
+
+        Set permissions on a network to a list of users, identifed by 
+        their usernames. The read flag ('Y' or 'N') sets read access, the write
+        flag sets write access. If the read flag is 'N', then there is
+        automatically no write access or share access.
+
+        Args:
+            network_id (int): The ID of the network to share
+            usernames  (List(String)): The list of usernames with whom to share the network
+            read       (string) (Y or N): Can the users read the network?
+            write      (string) (Y or N): Can the users edit the network?
+            share      (string) (optional) (Y or N): Can the users share the network with other users? This only goes 1 level deep, so if you share a network with a user and give them sharing ability, they cannot then set the 'share' flag to 'Y' when sharing with someone else.
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the network is not found
+            ResourceNotFoundError: If one of the usernames is incorrect or does not exist
+
         """
 
         sharing.set_network_permission(network_id,
@@ -109,12 +155,25 @@ class SharingService(HydraService):
          _returns=String)
     def hide_dataset(ctx, dataset_id, exceptions, read, write, share):
         """
-            Hide a particular piece of data so it can only be seen by its owner.
-            Only an owner can hide (and unhide) data.
-            Data with no owner cannot be hidden.
-            
-            The exceptions paramater lists the usernames of those with permission to view the data
-            read, write and share indicate whether these users can read, edit and share this data.
+
+        Hide a particular piece of data so it can only be seen by its owner.
+        Only an owner can hide (and unhide) data.
+        Data with no owner cannot be hidden.
+       
+        Args:
+            dataset_id (int): The ID of the dataset to be hidden
+            exceptions  (List(String)): the usernames of those with permission to view the data
+            read       (string) (Y or N): Can the users read the dataset?
+            write      (string) (Y or N): Can the users edit the dataset?
+            share      (string) (Y or N): Can the users share the dataset with other users? This only goes 1 level deep, so if you share a network with a user and give them sharing ability, they cannot then set the 'share' flag to 'Y' when sharing with someone else.
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the dataset is not found
+            HydraError: If the request is being made by a non-owner of the dataset
+
         """
         sharing.hide_dataset(dataset_id,
                              exceptions,
@@ -129,8 +188,19 @@ class SharingService(HydraService):
          _returns=String)
     def unhide_dataset(ctx, dataset_id):
         """
-            Un Hide a particular piece of data so it can only be seen by its owner.
-            Only an owner can hide (and unhide) data.
+        Un Hide a particular piece of data so it can only be seen by its owner.
+        Only an owner can hide (and unhide) data.
+
+        Args:
+            dataset_id (int): The ID of the dataset to be un-hidden 
+
+        Returns:
+            string: 'OK'
+
+        Raises:
+            ResourceNotFoundError: If the dataset is not found
+            HydraError: If the request is being made by a non-owner of the dataset
+
         """
         sharing.unhide_dataset(dataset_id,
                              **ctx.in_header.__dict__)
