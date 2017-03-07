7c22f202a0cff779d0ad1b4f6fee2f084adc99e2
Stephen Knox <knoxsp@gmail.com>
2017-03-07 16:42:38 +0000

    Update the app running code to work on linux.
    (cherry picked from commit d9a0b5c8cd47e84e704754b316e5e866657e8f8a)

--- HydraServer/python/HydraServer/ui/code/app_utilities.py
+++ HydraServer/python/HydraServer/ui/code/app_utilities.py
@@ -38,13 +38,13 @@ def create_zip_file(path, zip_file_name):
         for root, directories, files in files_directories_list:
             for directory in directories:
                 real_path = os.path.join(root, directory)
-                zip_path = real_path.replace(base_directory + '\\','')
+                zip_path = real_path.replace(base_directory + os.sep,'')
                 zip_file.write(real_path, zip_path)
             for file_name in files:
                 if file_name==os.path.basename(zip_file_name):
                     continue
                 real_path = os.path.join(root, file_name)
-                zip_path = real_path.replace(base_directory + '\\','')
+                zip_path = real_path.replace(base_directory + os.sep,'')
                 zip_file.write(real_path, zip_path)
     except Exception as e:
         print e
@@ -113,3 +113,17 @@ def get_progress_from_output(output):
                 status='Running'
                 break
     return  status, progress, total
+	
+def get_apps_properties(app_name):
+    f = open(os.path.dirname(os.path.realpath(__file__))+ os.sep + "apps.json", 'r')
+    json_string = ""
+    while 1:
+        line = f.readline()
+        if not line: break
+        json_string += line
+    f.close()
+    apps = json.loads(json_string)
+    for app_ in apps:
+        if app_['name']==app_name:
+            return app_
+    return None
--- HydraServer/python/HydraServer/ui/code/run_hydra_app.py
+++ HydraServer/python/HydraServer/ui/code/run_hydra_app.py
@@ -2,33 +2,29 @@ import subprocess
 import psutil
 import os
 from app_utilities import check_process_output, get_progress_from_output
+import logging
+log = logging.getLogger(__name__)
 
 '''
 run back ground process using the command and redircet the output to log file to be read later to get process progress ...
 '''
 
 def run_app(exe, args, use_wd=True):
-    arg = ''
-    for item in args.keys():
-        arg = arg + ' -' + item + ' ' + args[item]
-    cmd = exe + ' ' + arg
+    cmd = exe.split(' ') 
+    for item in args:
+        cmd.append('-'+item)
+        cmd.append(args[item])
+
     if use_wd==True:
         print args, '----------------------------->'
         os.chdir(os.path.dirname(exe))
 
-    f = open("..\\..\\log.txt", "w")
+    f = open(os.path.join(".."+os.sep, ".."+os.sep,"log.txt"), "w")
 
-    proc = subprocess.Popen(cmd, stdout=f, shell=True)
-    print "Process id: ", proc.pid
-    return proc.pid
-
-    '''
-    cmd = "python C:\work\csv_reader\process_tester.py"
-    f = open("c:\\temp\\blah.txt", "w")
     proc = subprocess.Popen(cmd, stdout=f)
+    print proc
     print "Process id: ", proc.pid
     return proc.pid
-   '''
 
 '''
 get running proces by its id and return its status i.e. still running or not
@@ -41,11 +37,14 @@ def get_app_progress(pid):
     total=100
     isIt=False
     for proc in psutil.process_iter():
-        if proc.pid == pid:
+        if proc.pid == pid and proc.status() != 'zombie':
+            log.info("%s is found ===============> %s", pid, proc.status())
             isIt=True
-            print pid , " is found ===============>", proc.status()
             break
-    with open("..\\..\\log.txt") as f:
+
+    logfile = os.path.join(".."+os.sep, ".."+os.sep, "log.txt")
+
+    with open(logfile) as f:
         contents = f.read().splitlines()
 
     if(isIt == True):
