4405f3a6f952a70dedfac104b74a8bcccd9b2995
Stephen Knox <knoxsp@gmail.com>
2017-09-20 08:49:31 +0100

    Handle addition of empty node, link and groups in add_network

--- HydraServer/python/HydraServer/lib/network.py
+++ HydraServer/python/HydraServer/lib/network.py
@@ -292,7 +292,7 @@ def _add_nodes(net_i, nodes):
     node_id_map = dict()
 
     if nodes is None or len(nodes) == 0:
-        return node_id_map, node_attrs
+        return node_id_map, node_attrs, {}
 
     _add_nodes_to_database(net_i, nodes)
 
@@ -344,7 +344,7 @@ def _add_links(net_i, links, node_id_map):
     link_id_map = dict()
 
     if links is None or len(links) == 0:
-        return link_id_map, link_attrs
+        return link_id_map, link_attrs, {}
 
     #Then add all the links.
 #################################################################
@@ -378,7 +378,7 @@ def _add_resource_groups(net_i, resourcegroups):
     grp_datasets = []
 
     if resourcegroups is None or len(resourcegroups)==0:
-        return group_id_map, group_attrs
+        return group_id_map, group_attrs, {} 
     #Then add all the groups.
     log.info("Adding groups to network")
     group_dicts = []
