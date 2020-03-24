6524e09fd1b029855037a434702857ff85e40268
Stephen Knox <stephen.knox@manchester.ac.uk>
2020-03-24 21:20:25 +0000

    Update the documentation config file

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ sys.path.insert(0, os.path.abspath('./HydraPlugins/WaterML/ImportWML'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.pngmath', 'sphinx.ext.ifconfig', 'alabaster', 'sphinxcontrib.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.imgmath', 'sphinx.ext.ifconfig', 'alabaster']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
